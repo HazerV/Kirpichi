43d4d3412083d62d0fb278427620fe23140d3e6e
HazerV <krotkov2004@icloud.com>
2024-05-07 19:30:26 +0400

    new

--- src/Components/HistoriesBlock/HistoriesBLock.jsx
+++ src/Components/HistoriesBlock/HistoriesBLock.jsx
@@ -1,19 +1,32 @@
 import ForStories from '../../assets/imgs/ForStories.png'
 import styles from './histories_block.module.scss'
-function HistoriesBLock () {
+import Arrow from '../../assets/icons/Arrow.svg'
+
+function HistoriesBLock() {
     return (
         <div className={styles.container}>
-            <div>
-                <p className={styles.text_block}>
+            <p className={styles.text_block}>
+                <h className={styles.text_block}>
                     Что мы делаем
-                </p>
-                <stories className={styles.block_stories}>
-                    <img src={ForStories} className={styles.image}/>
-                    <img src={ForStories} className={styles.image}/>
-                    <img src={ForStories} className={styles.image}/>
-                    <img src={ForStories} className={styles.image}/>
-                </stories>
-            </div>
+                </h>
+                <div className={styles.text_all}>
+                    <p>
+                        Все
+                    </p>
+                    <img src={Arrow}/>
+                </div>
+            </p>
+            <stories className={styles.block_stories}>
+                <img src={ForStories} className={styles.image}/>
+                <img src={ForStories} className={styles.image}/>
+                <img src={ForStories} className={styles.image}/>
+                <img src={ForStories} className={styles.image}/>
+                <img src={ForStories} className={styles.image}/>
+                <img src={ForStories} className={styles.image}/>
+                <img src={ForStories} className={styles.image}/>
+                <img src={ForStories} className={styles.image}/>
+                <img src={ForStories} className={styles.image}/>
+            </stories>
         </div>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fonts from './src/assets/fonts'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
